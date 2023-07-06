f8285a6e2112f7864f2790aeb589c10015ac061e
svolk19-stanford <59627836+svolk19-stanford@users.noreply.github.com>
2023-07-06 09:19:56 -0600

    changes to docking analysis

--- src/dock.py
+++ src/dock.py
@@ -4,6 +4,7 @@ import vina
 import pandas as pd 
 import meeko
 from rdkit import Chem
+from tqdm import tqdm
 
 affinities_df = pd.read_csv('../data/test.csv')
 affinities = list(affinities_df['G'])
@@ -14,17 +15,20 @@ scores = []
 energies = []
 # for i in range(82, len(ligands['smiles'])):
 #     lig = ligands['smiles'][i]
-for i, lig in enumerate(ligands['smiles']): 
+for i in tqdm(range(len(ligands['smiles']))):
+    lig = ligands['smiles'][i]
     mol = Chem.MolFromSmiles(lig)
     protonated_lig = Chem.AddHs(mol)
     Chem.AllChem.EmbedMolecule(protonated_lig)
     meeko_prep = meeko.MoleculePreparation()
     meeko_prep.prepare(protonated_lig)
-    lig_pdbqt = meeko_prep.write_pdbqt_file(f"../out/ligand_pdbqt_sia/ligand_{i + 1}.pdbqt")
-    print(f"prepared ligand {i}")
-    # v = vina.Vina(sf_name='vina', verbosity=0)
-    # v.set_receptor('../data/2hrl_rigid_fragment.pdbqt', '../data/2hrl_flex_fragment.pdbqt')
-    # v.set_ligand_from_string(lig_pdbqt)
+    lig_pdbqt = meeko_prep.write_pdbqt_string()
+    v = vina.Vina(sf_name='vina', verbosity=0)
+    v.set_receptor('../data/2hrl_rigid_multiple.pdbqt', '../data/2hrl_flex_multiple.pdbqt')
+    v.set_ligand_from_string(lig_pdbqt)
+    # from gnina
+    v.compute_vina_maps(center=[24.839, 30.851, 14.8775], box_size=[22.38, 26.076, 21.429])
+
     # bounding box for 2df3
     # v.compute_vina_maps(center=[18.192, 34.378, 23.055], box_size=[42, 42, 48])
     # bounding box for 2hrl: semi-flex
@@ -34,8 +38,8 @@ for i, lig in enumerate(ligands['smiles']):
     # # bounding box for 2hrl: fragment region only
     # v.compute_vina_maps(center=[32.395, 34.447, 10.547], box_size=[24, 28, 36])
     # v.optimize()
-    # v.dock(exhaustiveness=32, n_poses=20)
-    # output_pdbqt = v.poses(n_poses=20)
+    v.dock(exhaustiveness=32, n_poses=20)
+    output_pdbqt = v.poses(n_poses=20)
     # energy = v.energies(num_poses)
     # print(energy)
     # assert(False)
@@ -52,6 +56,15 @@ for i, lig in enumerate(ligands['smiles']):
     # scores.append(score)
     # output_scores = pd.DataFrame(scores, columns=[str(j) for j in range(num_poses)])
     # output_scores.to_csv("../out/output_scores_2HRL_fragment_32.csv")
+
+    pmol = meeko.PDBQTMolecule(output_pdbqt)
+    f = Chem.SDWriter(f'../out/subst_sia_sdfs/docked_poses_{i}.sdf')
+    for pose in pmol:
+        output_rdmol = pmol.export_rdkit_mol()
+        output_rdmol_w_bond_order = Chem.AllChem.AssignBondOrdersFromTemplate(
+            lig, output_rdmol)
+        f.write(output_rdmol_w_bond_order)
+    f.close()
 
     # y_hat = [j[0] for j in energies]
     # y = affinities[:(i + 1)]
